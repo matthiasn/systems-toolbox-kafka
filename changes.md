7e85c0617fbbdc246de32fff69fecc33064b7867
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-12-30 15:17:39 +0100

    v0.1.1: Kafka consumer and producer moved from systems-toolbox

---
+++ changes.md
@@ -0,0 +1,7 @@
+## v0.1.1 - December 30th, 2015
+
+```clojure
+[matthiasn/systems-toolbox-kafka "0.1.1"]
+```
+
+* Kafka consumer and producer moved from systems-toolbox into separate repo
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject matthiasn/systems-toolbox-kafka "0.1.1"
+  :description "Kafka producer and consumer components for systems-toolbox"
+  :url "https://github.com/matthiasn/systems-toolbox"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :source-paths ["src/clj"]
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [clj-kafka "0.3.4"]
+                 [com.taoensso/nippy "2.11.0-beta1"]]
+
+  :plugins [[lein-codox "0.9.1" :exclusions [org.clojure/clojure]]])
